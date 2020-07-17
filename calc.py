15b5dbbbb3891b4d73707bb66e75490608dac24c
hisban <a1111sc0513@163.com>
2020-07-17 15:08:26 +0800

    1st commit

---
+++ calc.py
@@ -0,0 +1,11 @@
+'''
+@Descripttion: 
+@version: 
+@Author: Hisban
+@Date: 2020-07-17 15:03:28
+@LastEditors: Hisban
+@LastEditTime: 2020-07-17 15:04:49
+'''
+x = input("x = ")
+y = input("y = ")
+print(x + y)
